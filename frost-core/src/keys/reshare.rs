3d979aa112124026040f505acdb318f684df5538
lazyluis <luis@side.one>
2025-09-27 08:46:01 +0800

    add comments for functions

--- frost-core/src/keys/reshare.rs
+++ frost-core/src/keys/reshare.rs
@@ -11,8 +11,10 @@ use crate::{
 
 use super::{SecretShare, SigningShare};
 
-/// compute signing share for a new participant from an existing key package and old signing key
-/// only run on existing participant nodes. 
+/// select t participants from the old group to help compute the new share
+/// generate new partial shares for all participants in `new_id_set`
+/// the new polynomial g_i(0) = f_i(i), compute g_ij = g_i(j) for all j in new_id_set
+/// returns a map from new participant identifier to their new share
 pub fn compute_new_participant_subshare<C: Ciphersuite, R: RngCore + CryptoRng>(
     old_key_package: &KeyPackage<C>, 
     new_id_set: &[Identifier<C>],
@@ -20,25 +22,27 @@ pub fn compute_new_participant_subshare<C: Ciphersuite, R: RngCore + CryptoRng>(
     mut rng: R,
 ) -> Result<BTreeMap<Identifier<C>, SecretShare<C>>, Error<C>> {
 
-    // compute the lagrange coefficient for the old secret share
-    // let lamda_i = compute_lagrange_coefficient(&old_id_set, None, old_key_package.identifier().clone())?;
     let secret = SigningKey::from_scalar(old_key_package.signing_share().to_scalar()) ?;
 
     // generate a new polynomial with free term = secret
     let coefficients = super::generate_coefficients::<C, R>(new_min_signers as usize - 1, &mut rng);
     let (coefficients, commitment) = generate_secret_polynomial(&secret, new_id_set.len() as u16, new_min_signers, coefficients)?;
 
-    let gij: BTreeMap<Identifier<_>, SecretShare<C>> = new_id_set.iter().map(|new_id| {
+    let gij_map: BTreeMap<Identifier<_>, SecretShare<C>> = new_id_set.iter().map(|new_id| {
         // evaluate the polynomial at new_identifier to get the new share
         let restore_share = SigningShare::from_coefficients(&coefficients, *new_id);
         let secret_share = SecretShare::new(*new_id, restore_share, commitment.clone());
         (*new_id, secret_share)
     }).collect();
 
-    Ok(gij)
+    Ok(gij_map)
 }
 
 /// aggregate the recovered shares into a new key package and public key package for a new participant
+/// `helper_shares` is a map from helper identifier to the share they sent, should be at least t shares. t is the threshold of the old group.
+/// `my_identifier` is the identifier of the participant reconstructing their share
+/// `new_participants` is the set of all identifiers in the new group
+/// returns the new participant's KeyPackage and the PublicKeyPackage for the new group
 pub fn reconstruct_key_from_subshares<C: Ciphersuite>(helper_shares: BTreeMap<Identifier<C>, SecretShare<C>>, my_identifier: Identifier<C>, new_participants: &BTreeSet<Identifier<C>>) -> Result<(KeyPackage<C>, PublicKeyPackage<C>), Error<C>>{
 
     let mut recovered_signing_share = <<C::Group as Group>::Field>::zero();
