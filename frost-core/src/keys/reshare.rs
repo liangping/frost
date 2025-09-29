66929b647ac0810974949d77b211176db91ac95c
lazyluis <luis@side.one>
2025-09-29 16:34:43 +0800

    add knowledge proof

--- frost-core/src/keys/reshare.rs
+++ frost-core/src/keys/reshare.rs
@@ -3,13 +3,78 @@
 use alloc::collections::btree_set::BTreeSet;
 use alloc::collections::BTreeMap;
 use alloc::vec::Vec;
+use derive_getters::Getters;
 
-use crate::keys::{CoefficientCommitment, KeyPackage, VerifiableSecretSharingCommitment, generate_secret_polynomial, PublicKeyPackage};
+use crate::keys::dkg::{compute_proof_of_knowledge, verify_proof_of_knowledge};
+use crate::keys::{generate_secret_polynomial, CoefficientCommitment, KeyPackage, PublicKeyPackage, VerifiableSecretSharingCommitment, VerifyingShare};
 use crate::{
-    compute_lagrange_coefficient, Ciphersuite, CryptoRng, Error, Field, Group, Identifier, RngCore, SigningKey
+    compute_lagrange_coefficient, Ciphersuite, CryptoRng, Error, Field, Group, Identifier, RngCore, Signature, SigningKey, VerifyingKey
 };
 
 use super::{SecretShare, SigningShare};
+
+/// A reshare secret share consists of a secretgetterand a proof of possession of the old signing key
+#[derive(Clone, Debug, PartialEq, Eq, Getters)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "serde", serde(bound = "C: Ciphersuite"))]
+#[cfg_attr(feature = "serde", serde(deny_unknown_fields))]
+pub struct ReshareSecretShare<C: Ciphersuite> {
+    /// The new secret share for the participant
+    pub(crate) secret_share: SecretShare<C>,
+    /// A proof of possession of the old signing key
+    pub(crate) proof: Signature<C>,
+}
+
+impl <C> ReshareSecretShare<C>
+where
+    C: Ciphersuite,
+{
+    /// Create a new ReshareSecretShare
+    pub fn new(secret_share: SecretShare<C>, proof: Signature<C>) -> Self {
+        Self {
+            secret_share,
+            proof,
+        }
+    }
+
+    /// Verify the proof of possession of the old signing key
+    pub fn verify(&self) -> Result<(VerifyingShare<C>, VerifyingKey<C>), Error<C>> {
+        verify_proof_of_knowledge(self.secret_share.identifier, self.secret_share.commitment(), &self.proof)?;
+        // Also verify that the signing share is valid with respect to the commitment
+        self.secret_share.verify()
+    }
+
+    /// Get the signing share
+    pub fn signing_share(&self) -> &SigningShare<C> {
+        &self.secret_share.signing_share
+    }
+
+    /// Get the commitment
+    pub fn commitment(&self) -> &VerifiableSecretSharingCommitment<C> {
+        &self.secret_share.commitment
+    }
+    
+}
+
+#[cfg(feature = "serialization")]
+impl<C> ReshareSecretShare<C>
+where
+    C: Ciphersuite,
+{
+    /// Serialize the struct into a Vec.
+    pub fn serialize(&self) -> Result<Vec<u8>, Error<C>> {
+        use crate::serialization::Serialize;
+
+        Serialize::serialize(&self)
+    }
+
+    /// Deserialize the struct from a slice of bytes.
+    pub fn deserialize(bytes: &[u8]) -> Result<Self, Error<C>> {
+        use crate::serialization::Deserialize;
+
+        Deserialize::deserialize(bytes)
+    }
+}
 
 /// select t participants from the old group to help compute the new share
 /// generate new partial shares for all participants in `new_id_set`
@@ -20,7 +85,7 @@ pub fn compute_new_participant_subshare<C: Ciphersuite, R: RngCore + CryptoRng>(
     new_id_set: &[Identifier<C>],
     new_min_signers: u16,
     mut rng: R,
-) -> Result<BTreeMap<Identifier<C>, SecretShare<C>>, Error<C>> {
+) -> Result<BTreeMap<Identifier<C>, ReshareSecretShare<C>>, Error<C>> {
 
     let secret = SigningKey::from_scalar(old_key_package.signing_share().to_scalar()) ?;
 
@@ -28,12 +93,14 @@ pub fn compute_new_participant_subshare<C: Ciphersuite, R: RngCore + CryptoRng>(
     let coefficients = super::generate_coefficients::<C, R>(new_min_signers as usize - 1, &mut rng);
     let (coefficients, commitment) = generate_secret_polynomial(&secret, new_id_set.len() as u16, new_min_signers, coefficients)?;
 
-    let gij_map: BTreeMap<Identifier<_>, SecretShare<C>> = new_id_set.iter().map(|new_id| {
+    let mut gij_map: BTreeMap<Identifier<_>, ReshareSecretShare<C>> = BTreeMap::new();
+    for new_id in new_id_set.iter() {
+        let proof = compute_proof_of_knowledge(*new_id, &coefficients, &commitment, &mut rng)?;
         // evaluate the polynomial at new_identifier to get the new share
         let restore_share = SigningShare::from_coefficients(&coefficients, *new_id);
         let secret_share = SecretShare::new(*new_id, restore_share, commitment.clone());
-        (*new_id, secret_share)
-    }).collect();
+        gij_map.insert(*new_id, ReshareSecretShare::new(secret_share, proof));
+    };
 
     Ok(gij_map)
 }
@@ -43,7 +110,7 @@ pub fn compute_new_participant_subshare<C: Ciphersuite, R: RngCore + CryptoRng>(
 /// `my_identifier` is the identifier of the participant reconstructing their share
 /// `new_participants` is the set of all identifiers in the new group
 /// returns the new participant's KeyPackage and the PublicKeyPackage for the new group
-pub fn reconstruct_key_from_subshares<C: Ciphersuite>(helper_shares: BTreeMap<Identifier<C>, SecretShare<C>>, my_identifier: Identifier<C>, new_participants: &BTreeSet<Identifier<C>>) -> Result<(KeyPackage<C>, PublicKeyPackage<C>), Error<C>>{
+pub fn reconstruct_key_from_subshares<C: Ciphersuite>(helper_shares: BTreeMap<Identifier<C>, ReshareSecretShare<C>>, my_identifier: Identifier<C>, new_participants: &BTreeSet<Identifier<C>>) -> Result<(KeyPackage<C>, PublicKeyPackage<C>), Error<C>>{
 
     let mut recovered_signing_share = <<C::Group as Group>::Field>::zero();
     let mut commitments = vec![];
--- frost-secp256k1-tr/src/keys/reshare.rs
+++ frost-secp256k1-tr/src/keys/reshare.rs
@@ -4,9 +4,9 @@ use alloc::collections::btree_map::BTreeMap;
 use alloc::collections::btree_set::BTreeSet;
 
 use crate::keys::{KeyPackage, PublicKeyPackage};
-use crate::{frost, Identifier, Error, CryptoRng, RngCore };
+use crate::{frost, CryptoRng, Error, Identifier, RngCore, Secp256K1Sha256TR };
 
-use super::SecretShare;
+type ReshareSecretShare = frost_core::keys::reshare::ReshareSecretShare<Secp256K1Sha256TR>;
 
 /// 
 pub fn compute_new_participant_subshare<R: RngCore + CryptoRng>(
@@ -14,11 +14,11 @@ pub fn compute_new_participant_subshare<R: RngCore + CryptoRng>(
     new_id_set: &[Identifier], 
     new_min_signers: u16,
     mut rng: R,
-) -> Result<BTreeMap<Identifier, SecretShare>, Error> {
+) -> Result<BTreeMap<Identifier, ReshareSecretShare>, Error> {
     frost::keys::reshare::compute_new_participant_subshare(old_key_package, new_id_set, new_min_signers, &mut rng )
 }
 
 /// 
-pub fn reconstruct_key_from_subshares(helper_shares: BTreeMap<Identifier, SecretShare>, my_identifier: Identifier, new_participants: &BTreeSet<Identifier>) -> Result<(KeyPackage, PublicKeyPackage), Error>{
+pub fn reconstruct_key_from_subshares(helper_shares: BTreeMap<Identifier, ReshareSecretShare>, my_identifier: Identifier, new_participants: &BTreeSet<Identifier>) -> Result<(KeyPackage, PublicKeyPackage), Error>{
     frost::keys::reshare::reconstruct_key_from_subshares(helper_shares, my_identifier, new_participants)
 }
